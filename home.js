ff62aa20cea5b17495a18095a3b593177ffbe584
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-04-27 23:32:53 -0300

    Home: Overlay Notifications

--- home.js
+++ home.js
@@ -1,7 +1,13 @@
 let elemAddNewIcon = document.querySelector(".add-new-icon");
 let elemCrearPublicacion = document.querySelector(".crear-publicacion");
 let oculto = true;
+let elemViewNotifications = document.querySelector('.notifications');
+let elemOverlayNotifications = document.querySelector('.overlay-notifications');
+let notificationsHidden = true;
+
 elemAddNewIcon.addEventListener("click", mostrarCrearPublicacion);
+elemViewNotifications.addEventListener('click', mostrarNotifications);
+
 function mostrarCrearPublicacion() {
   if (oculto) {
     elemCrearPublicacion.classList.add(
@@ -10,9 +16,34 @@ function mostrarCrearPublicacion() {
       "animate__faster",
       "crear-visible"
     );
-    oculto = !oculto;
+    oculto = false;
   } else {
-    elemCrearPublicacion.classList.add("animate__fadeOutRight", "crear-oculto");
-    oculto = !oculto;
+    elemCrearPublicacion.classList.remove(
+      "animate__animated",
+      "animate__fadeInRight",
+      "animate__faster",
+      "crear-visible"
+    );
+    oculto = true;
+  }
+}
+
+function mostrarNotifications() {
+  if (notificationsHidden) {
+    elemOverlayNotifications.classList.add(
+      "animate__animated",
+      "animate__fadeInRight",
+      "animate__faster",
+      "overlay-notifications-visible"
+    );
+    notificationsHidden = false;
+  } else {
+    elemOverlayNotifications.classList.remove(
+      "animate__animated",
+      "animate__fadeInRight",
+      "animate__faster",
+      "overlay-notifications-visible"
+    );
+    notificationsHidden = true;
   }
 }
